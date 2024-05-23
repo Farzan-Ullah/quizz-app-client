28f7b8eafc6681e30758e90ced66d35afaa48ec8
farzan <farzanullah07@gmail.com>
2024-05-23 14:06:42 +0530

    profilepage done and deployed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
-// import loginUser from "./components/LoginUser/LoginUser";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProfilePage />} />
-        {/* <Route path="/login" Component={loginUser} /> */}
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
